a2beaa73a0bb39b65f814d49c8ec0b9b3b52935f
Codax <108903742+Tsakila@users.noreply.github.com>
2025-05-10 22:03:50 +0200

    My Project files upload
    Added Project Files

---
+++ My Project/Form1.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnExercise1 = New Button()
+        Label1 = New Label()
+        btnExercise2 = New Button()
+        Label2 = New Label()
+        btnExercise3 = New Button()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        btnExercise4 = New Button()
+        Label8 = New Label()
+        btnExercise5 = New Button()
+        Label9 = New Label()
+        btnExercise6 = New Button()
+        Label10 = New Label()
+        btnExercise7 = New Button()
+        Label11 = New Label()
+        btnExercise8 = New Button()
+        Label12 = New Label()
+        SuspendLayout()
+        ' 
+        ' btnExercise1
+        ' 
+        btnExercise1.Location = New Point(27, 65)
+        btnExercise1.Name = "btnExercise1"
+        btnExercise1.Size = New Size(138, 35)
+        btnExercise1.TabIndex = 1
+        btnExercise1.Text = "Exercise 1"
+        btnExercise1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(178, 75)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(423, 20)
+        Label1.TabIndex = 2
+        Label1.Text = "Output each item in a separate message box one after another"
+        ' 
+        ' btnExercise2
+        ' 
+        btnExercise2.Location = New Point(27, 123)
+        btnExercise2.Name = "btnExercise2"
+        btnExercise2.Size = New Size(138, 35)
+        btnExercise2.TabIndex = 1
+        btnExercise2.Text = "Exercise 2"
+        btnExercise2.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(177, 130)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(449, 20)
+        Label2.TabIndex = 2
+        Label2.Text = "Output all  of the items in the same message box on separate lines"
+        ' 
+        ' btnExercise3
+        ' 
+        btnExercise3.Location = New Point(27, 173)
+        btnExercise3.Name = "btnExercise3"
+        btnExercise3.Size = New Size(138, 35)
+        btnExercise3.TabIndex = 1
+        btnExercise3.Text = "Exercise 3"
+        btnExercise3.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(177, 182)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(396, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Add up all of the items then output total in a message box"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(27, 9)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(574, 41)
+        Label4.TabIndex = 2
+        Label4.Text = "Coding exercises using array with loops"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(866, 56)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(363, 46)
+        Label5.TabIndex = 3
+        Label5.Text = "Begin each procedure with the following code" & vbCrLf & "to declare an initialize an array of integers" & vbCrLf
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(866, 141)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(200, 23)
+        Label6.TabIndex = 3
+        Label6.Text = "Dim aiData(4) As Integer" & vbCrLf
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(872, 197)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(120, 115)
+        Label7.TabIndex = 3
+        Label7.Text = "aiData(0)  = 5" & vbCrLf & "aiData(1) = 7" & vbCrLf & "aiData(2) = 33" & vbCrLf & "aiData(3) = 22" & vbCrLf & "aiData(4) = 8"
+        ' 
+        ' btnExercise4
+        ' 
+        btnExercise4.Location = New Point(27, 233)
+        btnExercise4.Name = "btnExercise4"
+        btnExercise4.Size = New Size(138, 35)
+        btnExercise4.TabIndex = 1
+        btnExercise4.Text = "Exercise 4"
+        btnExercise4.UseVisualStyleBackColor = True
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(177, 240)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(453, 20)
+        Label8.TabIndex = 2
+        Label8.Text = "Calculate the average of the items then output it on a message box"
+        ' 
+        ' btnExercise5
+        ' 
+        btnExercise5.Location = New Point(27, 284)
+        btnExercise5.Name = "btnExercise5"
+        btnExercise5.Size = New Size(138, 35)
+        btnExercise5.TabIndex = 1
+        btnExercise5.Text = "Exercise 5"
+        btnExercise5.UseVisualStyleBackColor = True
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(177, 291)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(493, 20)
+        Label9.TabIndex = 2
+        Label9.Text = "Add up the items greater than 20 then output the result in a message box"
+        ' 
+        ' btnExercise6
+        ' 
+        btnExercise6.Location = New Point(27, 343)
+        btnExercise6.Name = "btnExercise6"
+        btnExercise6.Size = New Size(138, 35)
+        btnExercise6.TabIndex = 1
+        btnExercise6.Text = "Exercise 6"
+        btnExercise6.UseVisualStyleBackColor = True
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(177, 350)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(358, 20)
+        Label10.TabIndex = 2
+        Label10.Text = "Find the largest item then output it in a message box" & vbCrLf
+        ' 
+        ' btnExercise7
+        ' 
+        btnExercise7.Location = New Point(27, 401)
+        btnExercise7.Name = "btnExercise7"
+        btnExercise7.Size = New Size(138, 35)
+        btnExercise7.TabIndex = 1
+        btnExercise7.Text = "Exercise 7"
+        btnExercise7.UseVisualStyleBackColor = True
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(177, 408)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(367, 40)
+        Label11.TabIndex = 2
+        Label11.Text = "Find the smallest item then output it in a message box" & vbCrLf & vbCrLf
+        ' 
+        ' btnExercise8
+        ' 
+        btnExercise8.Location = New Point(27, 463)
+        btnExercise8.Name = "btnExercise8"
+        btnExercise8.Size = New Size(138, 35)
+        btnExercise8.TabIndex = 1
+        btnExercise8.Text = "Exercise 8"
+        btnExercise8.UseVisualStyleBackColor = True
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(177, 470)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(628, 60)
+        Label12.TabIndex = 2
+        Label12.Text = "Replace each item in the array with a new value that is twice as big and output the new values" & vbCrLf & vbCrLf & vbCrLf
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1277, 627)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label12)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label4)
+        Controls.Add(Label1)
+        Controls.Add(btnExercise8)
+        Controls.Add(btnExercise7)
+        Controls.Add(btnExercise6)
+        Controls.Add(btnExercise5)
+        Controls.Add(btnExercise4)
+        Controls.Add(btnExercise3)
+        Controls.Add(btnExercise2)
+        Controls.Add(btnExercise1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents btnExercise1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnExercise2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnExercise3 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnExercise4 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents btnExercise5 As Button
+    Friend WithEvents Label9 As Label
+    Friend WithEvents btnExercise6 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnExercise7 As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents btnExercise8 As Button
+    Friend WithEvents Label12 As Label
+
+End Class
---
+++ My Project/Form1.vb
@@ -0,0 +1,210 @@
+﻿Public Class Form1
+    Private Sub btnExercise1_Click(sender As Object, e As EventArgs) Handles btnExercise1.Click
+
+        'Exercise 1: Output each item in a separate message box one after another
+
+        Dim aiData(4) As Integer 'Initialize the array of integers
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer  'initialize a counter (i)
+
+        'use a for loop to count the items and display them in a message box
+        For i = 0 To 4
+            MsgBox(aiData(i))
+        Next
+
+
+    End Sub
+
+    Private Sub btnExercise2_Click(sender As Object, e As EventArgs) Handles btnExercise2.Click
+
+        'Exercise 2: Output all  of the items in the same message box on separate lines
+
+        Dim aiData(4) As Integer 'Initialize the array of integers
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer  'initialize a counter (i)
+        Dim stOut As String 'string to outp the items
+
+        'use a for loop to count the items and display them in a message box
+        For i = 0 To 4
+            stOut = stOut & aiData(i) & vbNewLine
+        Next
+
+        MsgBox(stOut) 'show output
+    End Sub
+
+    Private Sub btnExercise3_Click(sender As Object, e As EventArgs) Handles btnExercise3.Click
+
+        'Exercise 3: Add up all the items then output the total in a message box
+
+        Dim aiData(4) As Integer 'Initialize the array of integers
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer  'initialize a counter (i)
+        Dim iTotal As Integer
+
+        'use a for loop to count the items, add all of them and display them in a message box
+        For i = 0 To 4
+            iTotal = iTotal + aiData(i)
+        Next
+
+        MsgBox("The total is " & iTotal)   'show output of the total
+
+    End Sub
+
+    Private Sub btnExercise4_Click(sender As Object, e As EventArgs) Handles btnExercise4.Click
+
+        'Exercise 4: Calculate the average of the items then output it in a message box
+
+        Dim aiData(4) As Integer 'Initialize the array of integers
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer  'initialize a counter (i)
+        Dim iTotal As Integer 'intialize the total
+
+        ' use the for loop to get a total of the items and calculate the average
+        For i = 0 To 4
+            iTotal = iTotal + aiData(i)
+        Next
+
+        MsgBox("The average is " & iTotal / 5) 'show output of the average
+    End Sub
+
+    Private Sub btnExercise5_Click(sender As Object, e As EventArgs) Handles btnExercise5.Click
+
+        'Exercise 5: Add up the items greater than 20 then output the result in a message box
+
+        Dim aiData(4) As Integer 'Initialize the array of integers
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer  'initialize a counter (i)
+        Dim iTotal As Integer  'intialize the total
+
+        ' use the for loop to count the items 
+        For i = 0 To 4
+            ' use if statement to check for items greater than 20 ,if there is any add them to a total
+            If aiData(i) > 20 Then
+                iTotal = iTotal + aiData(i)
+            End If
+        Next
+
+        ' & -ampersand is a contatenator (adding the string and the integer to show output)
+
+        MsgBox("The total of the items bigger than 20 is " & iTotal)
+    End Sub
+
+    Private Sub btnExercise6_Click(sender As Object, e As EventArgs) Handles btnExercise6.Click
+        'Exercise 6: find the largest item then output it in a message box
+
+        Dim aiData(4) As Integer 'Initialize the array of integers
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer  'initialize a counter (i)
+        Dim iMax As Integer  'initialize iMax , which will hold the largest number
+
+        iMax = 0 ' initaliaze iMax as zero , assuming working positive items 
+
+        'use for loop to count for items and check which item is larger than the next item in the array
+        For i = 0 To 4
+            If aiData(i) > iMax Then
+                iMax = aiData(i)
+            End If
+        Next
+
+        MsgBox("The largest item is " & iMax)
+    End Sub
+
+    Private Sub btnExercise7_Click(sender As Object, e As EventArgs) Handles btnExercise7.Click
+        'Exercise 7: find the smallest item then output it in a message box
+
+        Dim aiData(4) As Integer 'Initialize the array of integers
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer  'initialize a counter (i)
+        Dim iMin As Integer  'initialize iMin , which will hold the smallest number
+
+        iMin = 10000 ' initaliaze iMin as 10 000 , assuming a large number  is bigger than any item in the array
+
+        'use for loop to count for items and check which item is smallest than the next item in the array
+        For i = 0 To 4
+            If aiData(i) < iMin Then
+                iMin = aiData(i)
+            End If
+        Next
+
+        MsgBox("The smallest item is " & iMin)
+    End Sub
+
+    Private Sub btnExercise8_Click(sender As Object, e As EventArgs) Handles btnExercise8.Click
+
+        'Exercise 8: Replace each item in the array with a new value which is twice as big then output the new values
+
+        Dim aiData(4) As Integer 'Initialize the array of integers
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer  'initialize a counter (i)
+        ' use the for loop to count each item and multiply it by 2
+        For i = 0 To 4
+            aiData(i) = aiData(i) * 2
+        Next
+
+        Dim stOut As String 'initialize stOut to output new values of the items
+
+        For i = 0 To 4
+            stOut = stOut & aiData(i) & vbNewLine
+
+        Next
+
+        MsgBox(stOut)
+
+    End Sub
+End Class
